cc6a93ba98fcd0067a0f58cbfc348444133ffac4
jeromecox <jeromecox@hotmail.com>
2022-11-28 10:05:53 -0600

    started JS

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const container = document.querySelector("#container");
+const todoForm = document.querySelector("#todoForm");
+const todoList = document.querySelector("#todoList");
+
+fetch ("http://localhost:3000/notes/", {
+  method: "GET",
+  headers: {"Content-Type": "application/json"},
+})
+
+function postNote (note) {
+  fetch("http://localhost:3000/notes/", {
+  method: "POST",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify({"title": "Hi", "body": "COOL"})
+})
+  .then (response => response.json())
+  .then (data => )
+}
+
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+}
+);
