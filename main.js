afe3e68663fe42afa7718576532570fa2973b605
jeromecox <jeromecox@hotmail.com>
2022-11-29 08:28:07 -0600

    starting edit button

--- main.js
+++ main.js
@@ -6,38 +6,35 @@ const url = "http://localhost:3000/notes/";
 
 getRequest();
 
+function createEl(type, classArray, parent) {
+  let newElement = document.createElement(type);
+  newElement.classList.add(...classArray);
+  parent.appendChild(newElement);
+  return newElement;
+}
+
 function makeCard(object) {
-  let card = document.createElement("div");
-  card.classList.add("card");
+  let card = createEl("div", ["card"], todoList);
   card.dataset.number = object.id;
 
-  let title = document.createElement("div");
-  title.classList.add("subCard");
+  let title = createEl("div", ["subCard"], card);
   let titleText = object.title;
   title.innerText = `${titleText}`;
 
-  let todoBody = document.createElement("div");
-  todoBody.classList.add("subCard");
+  let todoBody = createEl("div", ["subCard"], card);
   let bodyText = object.body;
   todoBody.innerText = `${bodyText}`;
 
-  let editBtn = document.createElement("button");
-  editBtn.classList.add("edit-btn");
+  let editBtn = createEl("button", ["edit-btn"], card);
   editBtn.innerText = "✍️";
 
-  let deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("delete-btn");
+  let deleteBtn = createEl("button", ["delete-btn"], card);
   deleteBtn.innerText = "X";
 
-  card.appendChild(title);
-  card.appendChild(todoBody);
-  card.appendChild(editBtn);
-  card.appendChild(deleteBtn);
-
-  todoList.appendChild(card);
-
   // editBtn.addEventListener("click", (event) => {
-  //   editRequest();
+  //   todoList.replaceChildren();
+  //   editRequest(card);
+  //   getRequest();
   // });
 
   deleteBtn.addEventListener("click", (event) => {
@@ -71,24 +68,23 @@ function postRequest() {
   }).then((response) => response.json());
 }
 
-// function editRequest() {
-//   fetch(url, {
-//     method: "PATCH",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({ title: "", body: "" }),
-//   })
-//     .then((response) => response.json())
-//     .then((data) => {});
-// }
-
-function deleteRequest(card) {
-  let urlNew = url + card.dataset.number;
-  console.log(urlNew);
-  fetch(urlNew, {
-    method: "DELETE",
+function editRequest(editObj) {
+  let urlEdit = url + editObj.dataset.number;
+  fetch(url, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title: "", body: "" }),
   })
-    // .then(todoList.replaceChildren())
-    .then((response) => response.json());
+    .then((response) => response.json())
+    .then((data) => {});
+}
+
+function deleteRequest(delObj) {
+  let urlDel = url + delObj.dataset.number;
+  console.log(`URL to delete: ${urlDel}`);
+  fetch(urlDel, {
+    method: "DELETE",
+  }).then((response) => response.json());
 }
 
 todoForm.addEventListener("submit", (event) => {
