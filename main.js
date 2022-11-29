76147c5dc97bf4bbbbdf8374684cf34b413d6290
jeromecox <jeromecox@hotmail.com>
2022-11-29 13:34:38 -0600

    edit button works

--- main.js
+++ main.js
@@ -6,41 +6,64 @@ const url = "http://localhost:3000/notes/";
 
 getRequest();
 
+todoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  console.log(inputTitle.value);
+  console.log(inputBody.value);
+
+  postRequest();
+
+  todoForm.reset();
+});
+
 function createEl(type, classArray, parent) {
   let newElement = document.createElement(type);
   newElement.classList.add(...classArray);
-  parent.appendChild(newElement);
+  if (parent) parent.appendChild(newElement);
   return newElement;
 }
 
 function makeCard(object) {
   let card = createEl("div", ["card"], todoList);
+  card.id = object.id;
   card.dataset.number = object.id;
 
-  let title = createEl("div", ["subCard"], card);
+  let title = createEl("div", ["subCard", "title"], card);
+  title.id = "title" + object.id;
   let titleText = object.title;
   title.innerText = `${titleText}`;
 
-  let todoBody = createEl("div", ["subCard"], card);
+  let todoBody = createEl("div", ["subCard", "body"], card);
+  todoBody.id = "body" + object.id;
   let bodyText = object.body;
   todoBody.innerText = `${bodyText}`;
 
   let editBtn = createEl("button", ["edit-btn"], card);
   editBtn.innerText = "✍️";
 
+  let saveBtn = createEl("button", ["save-btn"], false);
+  saveBtn.innerText = "💾";
+
   let deleteBtn = createEl("button", ["delete-btn"], card);
   deleteBtn.innerText = "X";
 
-  // editBtn.addEventListener("click", (event) => {
-  //   todoList.replaceChildren();
-  //   editRequest(card);
-  //   getRequest();
-  // });
+  editBtn.addEventListener("click", (event) => {
+    title.contentEditable = true;
+    todoBody.contentEditable = true;
+    card.replaceChild(saveBtn, editBtn);
+  });
+
+  saveBtn.addEventListener("click", (event) => {
+    title.contentEditable = false;
+    todoBody.contentEditable = false;
+    card.replaceChild(editBtn, saveBtn);
+    editRequest(card);
+  });
 
   deleteBtn.addEventListener("click", (event) => {
-    todoList.replaceChildren();
     deleteRequest(card);
-    getRequest();
+    todoList.removeChild(card);
   });
 }
 
@@ -65,38 +88,37 @@ function postRequest() {
       title: `${inputTitle.value}`,
       body: `${inputBody.value}`,
     }),
-  }).then((response) => response.json());
+  })
+    .then((response) => response.json())
+    .then((object) => {
+      console.log(object);
+      makeCard(object);
+    });
 }
 
 function editRequest(editObj) {
-  let urlEdit = url + editObj.dataset.number;
-  fetch(url, {
+  let urlEdit = url + editObj.id;
+  console.log(`URL to edit: ${urlEdit}`);
+
+  let newTitle = editObj.querySelector(".title").innerText;
+  console.log(`Updating: ${newTitle}`);
+  let newBody = editObj.querySelector(".body").innerText;
+  console.log(`Updating: ${newBody}`);
+
+  fetch(urlEdit, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ title: "", body: "" }),
-  })
-    .then((response) => response.json())
-    .then((data) => {});
+    body: JSON.stringify({
+      title: `${newTitle}`,
+      body: `${newBody}`,
+    }),
+  }).then((response) => response.json());
 }
 
 function deleteRequest(delObj) {
-  let urlDel = url + delObj.dataset.number;
+  let urlDel = url + delObj.id;
   console.log(`URL to delete: ${urlDel}`);
   fetch(urlDel, {
     method: "DELETE",
   }).then((response) => response.json());
 }
-
-todoForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  console.log(inputTitle.value);
-  console.log(inputBody.value);
-
-  todoList.replaceChildren();
-
-  postRequest();
-
-  getRequest();
-  todoForm.reset();
-});
